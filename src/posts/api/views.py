bc6705ff5b429fd9eaa553411b5d1d2de07bcb56
Prashant Pandey <prashantpandey071@gmail.com>
2017-08-26 14:36:35 +0530

    implemented Retrieve API view and Update,delete view

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -1,7 +1,34 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import (
+	DestroyAPIView,
+	ListAPIView,
+	UpdateAPIView,
+	RetrieveAPIView
+	)
 from posts.models import Post
-from .serializers import PostSerializer
+from .serializers import PostDetailSerializer,PostListSerializer
+
+class PostDetailAPIView(RetrieveAPIView):
+
+	queryset = Post.objects.all()
+	serializer_class = PostDetailSerializer
+	lookup_field = 'slug'
+
+class PostUpdateAPIView(UpdateAPIView):
+
+	queryset = Post.objects.all()
+	serializer_class = PostDetailSerializer
+	lookup_field = 'slug'
+
+class PostDeleteAPIView(DestroyAPIView):
+
+	queryset = Post.objects.all()
+	serializer_class = PostDetailSerializer
+	lookup_field = 'slug'
 
 class PostListAPIView(ListAPIView):
 	queryset = Post.objects.all()
-	serializer_class = PostSerializer
+	serializer_class = PostListSerializer
+
+
+
+
