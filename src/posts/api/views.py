3f3232207fa209e2831518629806a3231a410abc
Prashant Pandey <prashantpandey071@gmail.com>
2017-08-27 20:19:10 +0530

    implemented custom permission,filtering a queryset and pagination

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -1,3 +1,10 @@
+from django.db.models import Q
+
+from rest_framework.filters import(
+	SearchFilter,
+	OrderingFilter,
+	)
+
 from rest_framework.generics import (
 	CreateAPIView,
 	DestroyAPIView,
@@ -6,7 +13,19 @@ from rest_framework.generics import (
 	RetrieveAPIView,
 	RetrieveUpdateAPIView
 	)
+
+
+
+from rest_framework.permissions import (
+	AllowAny,
+	IsAuthenticated,
+	IsAdminUser,
+	IsAuthenticatedOrReadOnly,
+	)
+
+from .pagination import PostLimitOffsetPagination,PostPageNumberPagination
 from posts.models import Post
+from .permissions import IsOwnerOrReadOnly
 from .serializers import (
 	PostCreateUpdateAPIView,
 	PostDetailSerializer,
@@ -17,6 +36,7 @@ class PostCreateAPIView(CreateAPIView):
 
 	queryset = Post.objects.all()
 	serializer_class = PostCreateUpdateAPIView
+	permission_classes = [IsAuthenticated]
 
 	def perform_create(self,serializer):
 		serializer.save(user = self.request.user)
@@ -33,6 +53,8 @@ class PostUpdateAPIView(RetrieveUpdateAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostCreateUpdateAPIView
 	lookup_field = 'slug'
+	permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+
 
 	def perform_update(self,serializer):
 		serializer.save(user = self.request.user)
@@ -42,10 +64,26 @@ class PostDeleteAPIView(DestroyAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostDetailSerializer
 	lookup_field = 'slug'
+	permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
+
 
 class PostListAPIView(ListAPIView):
-	queryset = Post.objects.all()
 	serializer_class = PostListSerializer
+	filter_backends = [SearchFilter,OrderingFilter]
+	search_fields = ['title','content','user__first_name']
+	pagination_class= PostPageNumberPagination #PageNumberPagination
+
+	def get_queryset(self,*args,**kwargs):
+		queryset_list = Post.objects.all()
+		query = self.request.GET.get("q")
+		if query:
+			queryset_list = queryset_list.filter(
+					Q(title__icontains=query)|
+					Q(content__icontains=query)|
+					Q(user__first_name__icontains=query) |
+					Q(user__last_name__icontains=query)
+					).distinct()
+		return queryset_list 
 
 
 
