ee83c74e986dfe8ff03a7360f1f1aaa9e65e79c6
Prashant Pandey <prashantpandey071@gmail.com>
2017-08-30 23:11:33 +0530

    implemented comments api and comment children

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -1,4 +1,8 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import (
+	ModelSerializer,
+	HyperlinkedIdentityField,
+	SerializerMethodField,
+	)
 from posts.models import Post
 
 
@@ -15,25 +19,57 @@ class PostCreateUpdateAPIView(ModelSerializer):
 		]
 
 class PostDetailSerializer(ModelSerializer):
+	user = SerializerMethodField()
+	image = SerializerMethodField()
+	html = SerializerMethodField()
+
 	class Meta:
 		model = Post
 		fields = [
 			'id',
+			'user',
 			'title',
 			'slug',
 			'content',
+			'html',
 			'publish',
+			'image'
 
 		]
+	def get_html(self,obj):			
+		return obj.get_markdown()
+
+	def get_user(self,obj):
+		return str(obj.user.username)
+
+	def get_image(self,obj):
+		try:
+			image = obj.image.url
+		except:
+			image = None
+		return image
+		
+
+
 
 class PostListSerializer(ModelSerializer):
+	user = SerializerMethodField()
+	url = HyperlinkedIdentityField(
+		view_name='posts-api:detail',
+		lookup_field='slug'
+		)
+	
 	class Meta:
 		model = Post
 		fields = [
+			'url',
+			'user',
 			'title',
-			'slug',
 			'content',
 			'publish',
 
 		]
+
+	def get_user(self,obj):
+		return str(obj.user.username)
 
